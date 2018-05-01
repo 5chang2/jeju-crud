c8472f5b94cb1a2602b10e50a1292e416d4b1224
EC2 Default User <ec2-user@ip-172-31-12-61.us-east-2.compute.internal>
2018-05-01 06:58:36 +0000

    입력한 내용이 전달 되는지 확인을 위한 코드 & 오타 수정

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -1,7 +1,17 @@
 class BoardController < ApplicationController
+  
+  skip_before_action :verify_authenticity_token
+
   def index
   end
   
   def new
+  end
+  
+  def create
+    @title = params[:title]
+    @content = params[:content]
+    puts @title
+    puts @content
   end
 end
