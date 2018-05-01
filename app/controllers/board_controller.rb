0f74f310377c3073d1659e4350db815185198bc8
EC2 Default User <ec2-user@ip-172-31-12-61.us-east-2.compute.internal>
2018-05-01 06:47:48 +0000

    게시물 작성페이지를 위한 routes,controller,view 설정

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -1,4 +1,7 @@
 class BoardController < ApplicationController
   def index
   end
+  
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'board/index'
+  root 'board#index'
+  get '/new' => 'board#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
